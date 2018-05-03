150bc27cb57c3d4fa09b87cff19d5cba2e7dfd68
danx0r <dan.miller@hiqlabs.com>
2018-05-03 12:24:22 -0700

    ensure fetch all branches locally

--- getgit.py
+++ getgit.py
@@ -8,6 +8,24 @@ repo = args.repo
 
 print ("archiving:", repo)
 
+print ("retrieving repo")
+cmd = "git clone git@github.com:hiqlabs/%s.git" % repo
+print (cmd)
+os.system(cmd)
+
+os.chdir(repo)
+cmd = """git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done"""
+print (cmd)
+os.system(cmd)
+cmd = "git fetch --all"
+print (cmd)
+os.system(cmd)
+cmd = "git pull --all"
+print (cmd)
+os.system(cmd)
+
+os.chdir("..")
+
 print ("retrieving issues")
 cmd = "curl https://api.github.com/repos/hiqlabs/%s/issues?access_token=%s > %s_issues.json" % (repo, args.token, repo)
 print (cmd)
@@ -18,7 +36,11 @@ cmd = "git clone git@github.com:hiqlabs/%s.wiki" % repo
 print (cmd)
 os.system(cmd)
 
-print ("retrieving repo")
-cmd = "git clone git@github.com:hiqlabs/%s.git" % repo
+print ("coalescing")
+cmd = "mv %s.wiki %s" % (repo, repo)
+print (cmd)
+os.system(cmd)
+
+cmd = "mv %s_issues.json %s" % (repo, repo)
 print (cmd)
 os.system(cmd)
